4e83009e5ada8f71bf0262250e9d90cd4068a7a5
Robert Grasfield <120531353+Proton118@users.noreply.github.com>
2025-05-02 13:45:14 -0400

    Final Version

--- src/App.java
+++ src/App.java
@@ -5,10 +5,37 @@ and a text file that contains the entire story. Your job is to read the file lin
 line and use appropriate skills to see count the number of words. How you do this is up to you!
 */
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<String> words = new ArrayList<>();
+        int wordCount = 0;
+
+        try(Scanner scanner = new Scanner(Paths.get("src/greenEggsAndHam.txt"))){
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                line = line.replace('?', ' ');
+                line = line.replace('!', ' ');
+                line = line.replace('.', ' ');
+                line = line.replace(',', ' ');
+                line = line.replace('-', ' ');
+
+                String[] lineWords = line.split(" ");
+                for(String word : lineWords){
+                    word = word.toLowerCase();
+                    if(!words.contains(word) && !word.isEmpty()){
+                        words.add(word);
+                        wordCount++;
+                    }
+                }
+            }
+        }
+        System.out.println("The number of unique words in Green Eggs and Ham is: " + wordCount);
     }
 
 }
